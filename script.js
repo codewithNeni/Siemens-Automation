8ced6ccaff2bb8106cb7a049172fc57aca908c97
codewithNeni <168447970+codewithNeni@users.noreply.github.com>
2024-07-11 15:27:39 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-const hamburger = document.querySelector('.hamburger');
-const sidebar = document.querySelector('.sidebar');
-const closeSidebar = document.querySelector('.close-sidebar');
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const sidebar = document.querySelector('.sidebar');
+    const closeSidebar = document.querySelector('.close-sidebar');
 
-hamburger.addEventListener('click', () => {
-    sidebar.classList.add('active');
-    hamburger.classList.add('active');
-    closeSidebar.style.display = 'block';
+    hamburger.addEventListener('click', () => {
+        sidebar.classList.toggle('active');
+    });
+
+    closeSidebar.addEventListener('click', () => {
+        sidebar.classList.remove('active');
+    });
 });
-
-closeSidebar.addEventListener('click', () => {
-    sidebar.classList.remove('active');
-    hamburger.classList.remove('active');
-    closeSidebar.style.display = 'none';
-});
-
