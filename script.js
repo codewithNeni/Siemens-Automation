1c3580628b7c293db221c9ab22047d43eb7b413e
codewithNeni <168447970+codewithNeni@users.noreply.github.com>
2024-07-12 05:04:04 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-    document.querySelector('.hamburger').addEventListener('click', function() {
-        document.querySelector('.sidebar').classList.toggle('active');
-    });
 
-    document.querySelector('.close-sidebar').addEventListener('click', function() {
-        document.querySelector('.sidebar').classList.remove('active');
-    });
-
-    // Close sidebar when a link is clicked
-    document.querySelectorAll('.sidebar-links a').forEach(link => {
-        link.addEventListener('click', function() {
-            document.querySelector('.sidebar').classList.remove('active');
-        });
-    });
